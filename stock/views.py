385c874e715126308d88a3a04fdd603c32d3f479
Deepak <95750118+deepak8299@users.noreply.github.com>
2023-03-20 03:19:10 +0530

    h
    gh

---
+++ stock/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request,'stocklist.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'signin.html', {'form': form})
+
+def signout(request):
+    logout(request)
+    return redirect('home')
